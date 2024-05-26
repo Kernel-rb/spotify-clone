982d29c4e4d37c1c7743cc4fd297276b812b6ba7
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-26 17:34:49 +0200

    feat : Authentification modal && function

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -4,29 +4,53 @@ import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-rea
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 import Modal from "./Modal";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useEffect } from "react";
 
 
 
 const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const router = useRouter();
-    const {session} = useSessionContext();
+    const { session } = useSessionContext();
+    const { onClose, isOpen } = useAuthModal();
+
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [ session , router, onClose]);
+    
+    const onChange = (open: boolean) => {
+        if (!open) onClose();
+    }
   return (
     <Modal
       title="welcome back!"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
-          <Auth
-              supabaseClient={supabaseClient}
-              appearance={
-                {
-                  theme: ThemeSupa
-                }
-              }
-          />
+      <Auth
+        theme="dark"
+        magicLink={true}
+        providers={["google", "facebook"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
+        }}
+      />
     </Modal>
   );
 }
