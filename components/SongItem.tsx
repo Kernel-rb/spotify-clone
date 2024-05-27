c19f4d2b7af14036acc96f965be5c8553b0bd6a4
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-27 18:13:01 +0200

    feat: Add song Fetch and list display

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import Image from "next/image";
 
 interface SongItemProps {
   data: Song;
@@ -35,7 +36,53 @@ const SongItem: React.FC<SongItemProps> = (
       p-3
     "
     >
-      Song iem
+      <div
+        className="
+      relative
+      aspect-square
+      w-full
+      h-full
+      rounded-md
+      overflow-hidden
+      "
+      >
+        <Image
+          className="object-cover"
+          src={imagePath || "/images/liked.png"}
+          fill
+          alt="Image"
+        />
+      </div>
+      <div
+        className="
+      flex
+      flex-col
+      items-start
+      w-full
+      p-4
+      gap-y-1
+      "
+      >
+        <p
+          className="
+        font-semibold
+        truncate
+        w-full
+        "
+        >
+          {data.title}
+        </p>
+        <p
+          className="
+        text-neutral-400
+        text-sm
+        pb-4
+        w-full
+        "
+        >
+          By {data.author}
+        </p>
+      </div>
     </div>
   );
 }
