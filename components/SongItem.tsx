97b592ffac7360d06aef0d4a4d464a07a1bc167c
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-27 18:00:03 +0200

    feat: Add song Fetch and list display

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 
 interface SongItemProps {
@@ -13,9 +14,30 @@ const SongItem: React.FC<SongItemProps> = (
         onClick
     }
 ) => {
+  const imagePath = useLoadImage(data);
   return (
-    <div>SongItem</div>
-  )
+    <div
+      onClick={() => onClick(data.id)}
+      className="
+      relative
+      group
+      flex
+      flex-col
+      items-center
+      justify-center
+      rounded-md
+      overflow-hidden
+      gap-x-4
+      bg-neutral-400/5
+      cursor-pointer
+      hover:bg-neutral-400/10
+      transition
+      p-3
+    "
+    >
+      Song iem
+    </div>
+  );
 }
 
 export default SongItem
