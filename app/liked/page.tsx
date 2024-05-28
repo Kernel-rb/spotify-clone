4eba105d9199ce18ccb0f0d8dea1ce80ae84e830
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-28 17:42:55 +0200

    feat: Favourite functionnality

---
+++ app/liked/page.tsx
@@ -0,0 +1,11 @@
+export const revalidate = 0;
+
+const Liked = () => {
+  return (
+      <div>
+          page
+      </div>
+  )
+}
+
+export default Liked
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,6 +4,7 @@ import SearchInput from "@/components/SearchInput";
 import SearchContent from "./components/SearchContent";
 
 
+export const revalidate = 0;
 
 interface SearchProps {
     searchParams: {
