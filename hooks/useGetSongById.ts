3ea0cfbb8cb5fa402cb11a29138d73450858ce5c
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-29 15:28:08 +0200

    feat: Player functionnality

---
+++ hooks/useGetSongById.ts
@@ -0,0 +1,36 @@
+import {  useEffect, useMemo, useState } from 'react';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { Song } from '@/types';
+import toast from 'react-hot-toast';
+
+
+
+const useGetSongById = (id?: string) => {
+    const [isLoading, setIsloading] = useState(false);
+    const [song, setSong] = useState<Song | undefined>(undefined);
+    const { supabaseClient } = useSessionContext();
+
+    useEffect(() => {
+        const fetchSong = async () => {
+            const { data, error } = await supabaseClient
+                .from("songs")
+                .select("*")
+                .eq("id", id)
+                .single();
+            if (error) {
+                setIsloading(false);
+                return toast.error(error.message);
+            }
+            setSong(data as Song);
+            setIsloading(false);
+        }
+        fetchSong();
+    }, [id, supabaseClient])
+    
+    return useMemo(() => ({
+        isLoading,
+        song
+    }), [isLoading, song])
+}
+
+export default useGetSongById;
